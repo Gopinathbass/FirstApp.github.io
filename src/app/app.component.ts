06e429acab7ffc915b71951b5d5cbeebc472d4c3
Gopinathbass <b.gopinath0000@gmail.com>
2023-10-30 21:59:52 +0530

    cretaed a post component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FirstApp';
+  PostArray:Array<string>=['post 1','post 2','post 3','Post 4','post 5'];
+  objArray:Array<any>=[
+    {id:1,postTitle:'Post 1'},
+  {id:2,postTitle:'Post 2'},
+  {id:3,postTitle:'Post 3'},
+  {id:4,postTitle:'Post 4'},
+  {id:5,postTitle:'Post 5'}
+];
+ constructor(){
+  for(let i=0;i<this.PostArray.length;i++){
+    console.log(this.PostArray[i]);
+    
+  }
+ }
 }
